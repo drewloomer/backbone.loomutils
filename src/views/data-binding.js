91ad3fa61609016b7dcc1adf6fcf74494ef7f979
Drew Loomer <drewloomer@gmail.com>
2013-11-16 18:17:21 -0500

    allow binding sources hash to use strings

--- src/views/data-binding.js
+++ src/views/data-binding.js
@@ -15,6 +15,57 @@ define([
 
 			NestedView.prototype.initialize.apply(this, arguments);
 			this.render();
+		},
+
+
+		/**
+		 * Override apply bindings to parse string data sources
+		 */
+		applyBindings: function () {
+
+			var bindingSources = {};
+
+			_.each(this.bindingSources, function (source, name) {
+				if (typeof source === 'string') {
+					bindingSources[name] = this.parseSourceName(source);
+				}
+				else {
+					bindingSources[name] = source;
+				}
+			}, this);
+			console.log(bindingSources);
+
+			this.bindingSources = bindingSources;
+
+			NestedView.prototype.applyBindings.apply(this, arguments);
+		},
+
+
+		/**
+		 * Parse a source string
+		 * @param {String} sourceName
+		 */
+		parseSourceName: function (sourceName) {
+
+			var parts = sourceName.split('.'),
+				i = 0,
+				len = parts.length,
+				source = this;
+
+			for (i; i < len; i++) {
+				if (source[parts[i]]) {
+					source = source[parts[i]];
+				}
+				else if (source.get && source.get(parts[i])) {
+					source = source.get(parts[i]);
+				}
+				else {
+					source = sourceName;
+					break;
+				}
+			}
+
+			return source;
 		}
 	});
 
