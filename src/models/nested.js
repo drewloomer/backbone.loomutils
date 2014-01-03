35a556230342f33118c48cba6e6ddfded786821f
Drew Loomer <drewloomer@gmail.com>
2014-01-02 20:48:02 -0500

    add event listeners to the base model

--- src/models/nested.js
+++ src/models/nested.js
@@ -10,10 +10,95 @@ define([
 
 	var M = Epoxy.Model.extend({
 
+		/**
+		 * Values to not JSONify
+		 * @type {Object}
+		 */
 		blacklist: {},
 
+
+		/**
+		 * Values to JSONify
+		 * @type {Object}
+		 */
 		whitelist: {},
 
+
+		/**
+		 * Initialize override
+		 */
+		initialize: function () {
+
+			// Add object listeners
+			this._setupListeners();
+
+
+			// Super
+			Epoxy.Model.prototype.initialize.apply(this, arguments);
+		},
+
+
+		/**
+		 * Setup listeners
+		 */
+		_setupListeners: function () {
+
+			_.each(this.listeners, this._setupListenerGroup, this);
+		},
+
+
+		/**
+		 * Setup a group of listeners
+		 * @param {Object} group
+		 * @param {String} name
+		 */
+		_setupListenerGroup: function (group, name) {
+
+			var obj;
+
+			if (name === 'this') {
+				obj = this;
+			}
+			else if (this.get(name)) {
+				obj = this.get(name)
+			}
+			else if (this[name]) {
+				obj = this[name];
+			}
+
+			if (!obj) {
+				return;
+			}
+
+			_.each(group, function (methodName, eventName) {
+				this._setupListener(obj, eventName, methodName);
+			}, this);
+		},
+
+
+		/**
+		 * Setup a listener listener
+		 * @param {Object} obj
+		 * @param {String} eventName
+		 * @param {String} methodName
+		 */
+		_setupListener: function (obj, eventName, methodName) {
+
+			var callback = this[methodName] || undefined;
+
+			if (!callback) {
+				return;
+			}
+
+			this.listenTo(obj, eventName, callback);
+		},
+
+
+		/**
+		 * Override to obey white and blacklists
+		 * @param  {Object} options
+		 * @return {Object}
+		 */
 		toJSON: function (options) {
 
 			var json = {};
